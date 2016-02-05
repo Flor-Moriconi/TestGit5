999555c44d1b4d187a3646c70b98afea12caa9bf
Florencia Moriconi <fmoriconi@yopapp.me>
2016-02-05 12:57:19 -0300

    add all files

---
+++ TestGit5/TestGit5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit5
+//
+//  Created by Florencia Moriconi on 5/2/16.
+//  Copyright © 2016 Florencia Moriconi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
